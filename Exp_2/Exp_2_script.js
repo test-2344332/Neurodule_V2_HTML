81e94f2c3ba971fbed3ca7e9361f23ef3a1622c6
Hadrien <hadriencorthay@gmail.com>
2026-05-10 14:02:25 +0200

    fin controles synapse

--- Exp_2/Exp_2_script.js
+++ Exp_2/Exp_2_script.js
@@ -1,25 +1,67 @@
 // taille labo ou 10'000
 let labo = false
-let curare = false
-let nervin = false
-let tetrotoxine = false
+
+let presencecurare = false
+let presencenervin = false
+let presencetetrodotoxine = false
 
 let btnlabo = document.getElementById("labo")
 let commandeslabo = document.getElementById("CommandesSpecifiqueslabo")
-let btn10000 = document.getElementById("10'000")
+let btn10000 = document.getElementById("zoom")
 let commandes10000 = document.getElementById("CommandesSpecifiques10000")
+
+let curarebtn = document.getElementById("curare")
+let nervinbtn = document.getElementById("nervin")
+let tetrodotoxinebtn = document.getElementById("tetrodotoxine")
+
+
 function passage10000(){
     labo = false;
     btn10000.style.display="none";
-    commandes10000.style.display="none";
+    commandes10000.style.display="block";
     btnlabo.style.display="block";
-    commandeslabo.style.display="block";
+    commandeslabo.style.display="none";
 }
 function passagelabo(){
     labo = true;
     btn10000.style.display="block";
-    commandes10000.style.display="block";
+    commandes10000.style.display="none";
     btnlabo.style.display="none";
-    commandeslabo.style.display="none";
+    commandeslabo.style.display="block";
 }
 
+function changecurare(){
+    if(presencecurare == false){
+        presencecurare = true
+        curarebtn.innerHTML = "retirer curare"
+    }
+    else{
+        presencecurare = false
+        curarebtn.innerHTML = "injecter curare"
+    }
+
+}
+
+function changenervin(){
+    if(presencenervin == false){
+        presencenervin = true
+        nervinbtn.innerHTML = "retirer nervin"
+    }
+    else{
+        presencenervin = false
+        nervinbtn.innerHTML = "injecter nervin"
+    }
+
+}
+
+function changetetrodotoxine(){
+    if(presencetetrodotoxine == false){
+        presencetetrodotoxine = true
+        tetrodotoxinebtn.innerHTML = "retirer tetrodotoxine"
+    }
+    else{
+        presencetetrodotoxine = false
+        tetrodotoxinebtn.innerHTML = "injecter tetrodotoxine"
+    }
+
+}
